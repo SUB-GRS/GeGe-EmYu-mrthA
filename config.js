b2ef0bb124649e78be5f59694642ab90a153be9c
SUB-GRS <putrapdika999@gmail.com>
2026-02-15 17:05:32 +0700

    Reset Repository (Clean Up)

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    geminikeys: [
-        "AIzaSyDybHll1xU-MKGPllZYYOG_ybcX6gNTyLw",
-        "AIzaSyAnnVHUd_gPkH-AsbvF_Y6ETPIMcXV_aLk",
-        "AIzaSyB_Token_3_Lu",
-        "AIzaSyB_Token_4_Lu",
-        "AIzaSyB_Token_5_Lu"
-    ]
-};
