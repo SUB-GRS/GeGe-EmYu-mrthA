1d7553b3825cbb80ed6b63f860dda73dc866ae36
SUB-GRS <putrapdika999@gmail.com>
2026-02-15 16:45:54 +0700

    Upload config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    geminikeys: [
+        "AIzaSyDybHll1xU-MKGPllZYYOG_ybcX6gNTyLw",
+        "AIzaSyAnnVHUd_gPkH-AsbvF_Y6ETPIMcXV_aLk",
+        "AIzaSyB_Token_3_Lu",
+        "AIzaSyB_Token_4_Lu",
+        "AIzaSyB_Token_5_Lu"
+    ]
+};
